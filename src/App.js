cbe91abdebf56b13e9767c1641ebe7d41aca9802
alanhg <qianghe421@163.com>
2018-07-31 22:46:55 +0800

    optimize

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-            <HelloWorld />
+            <HelloWorld name="alan" />
+            <HelloWorld name="dave" />
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
