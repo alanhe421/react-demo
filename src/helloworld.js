2826427a8ec5e355feeabc07b984fd5fe7a69b31
alanhg <qianghe421@163.com>
2018-07-31 22:34:23 +0800

    组件添加实现

---
+++ src/helloworld.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './helloworld.css';
+
+const HelloWorld = () => {
+    return (<div className="HelloWorld">Hello World!!!</div>);
+};
+
+export default HelloWorld;
