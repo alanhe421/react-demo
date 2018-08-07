ad2d8c47e67cc2092b3bee271cba5cd99e908957
alanhg <qianghe421@163.com>
2018-08-07 11:01:11 +0800

    路由跳转实现

--- src/App.js
+++ src/App.js
@@ -2,35 +2,39 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import HelloWorld from './helloworld';
 import './App.css';
+import {BrowserRouter, Link, Route} from 'react-router-dom'
+import TodoComponent from "./Todo";
 
 class App extends Component {
     a = 1;
 
     btnClicked() {
+        localStorage.setItem('a', this.a);
         alert(this.a);
     }
 
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <div className="App-intro">
-                    <HelloWorld name="alan"/>
-                    <HelloWorld name="dave"/>
-                    To get started, edit <code>src/App.js</code> and save to reload.
+            <BrowserRouter>
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <h1 className="App-title">Welcome to React</h1>
+                    </header>
+                    <div className="App-intro">
+                        <HelloWorld name="alan"/>
+                        <HelloWorld name="dave"/>
+                        To get started, edit <code>src/App.js</code> and save to reload.
+                    </div>
+                    {/*<Router>*/}
+                    {/*<a href="/todo">todo</a>*/}
+                    <Route path='/todo' component={TodoComponent}/>
+                    {/*<Route path='/hello' component={TodoComponent}/>*/}
+                    {/*</Router>*/}
+                    <Link to="/todo">Zillow Group</Link>
+                    <button onClick={this.btnClicked.bind(this)}>点击我</button>
                 </div>
-                {/*<Router>*/}
-                {/*<a href="/todo">todo</a>*/}
-                {/*<Route path='/todo' component={TodoComponent}/>*/}
-                {/*<Route path='/hello' component={TodoComponent}/>*/}
-                {/*</Router>*/}
-
-                <button onClick={this.btnClicked.bind(this)}>点击我</button>
-
-            </div>
+            </BrowserRouter>
         );
     }
 }
--- src/Todo/index.js
+++ src/Todo/index.js
@@ -1,15 +1,34 @@
 import React, {Component} from 'react'
+import {Redirect} from 'react-router-dom'
 
 export default class TodoComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {back: false};
+    }
+
+    backHome() {
+        this.setState({back: true});
+    }
+
     render() {
-        return (<ul>
-            <li>
-                1111
-            </li>
-            <li>
-                2222
-            </li>
-        </ul>)
+        if (this.state.back) {
+            return <Redirect to="/"/>
+        }
+        return (
+            <div>
+
+                <ul>
+                    <li>
+                        1111
+                    </li>
+                    <li>
+                        2222
+                    </li>
+                </ul>
+                <button onClick={this.backHome.bind(this)}>返回首页</button>
+            </div>
+        )
     }
 
 }
