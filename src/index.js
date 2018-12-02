70bfd1f533b07dc90b33252a00cd35410c06653a
alanhg <qianghe421@163.com>
2018-12-02 20:01:57 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Provider from 'react-redux/es/components/Provider';
+import {createStore} from 'redux';
+import './App.css';
+import App from './components/App';
+import rootReducer from './reducers'
 
-ReactDOM.render(<App/>, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>, document.getElementById('root'));
