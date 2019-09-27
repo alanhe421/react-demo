bab3b99aa201d5d694f21140135ac071cd330849
alanhg <qianghe421@gmail.com>
2019-09-27 12:00:25 +0800

    test antd modal

--- src/components/antd/antd-page.jsx
+++ src/components/antd/antd-page.jsx
@@ -1,47 +1,88 @@
-import React, {Component} from 'react';
-import {Table} from 'antd';
+import React from 'react';
+import {Button, Col, Modal, Row, Table} from 'antd';
+import {getBooks} from '../../api';
 
-class AntdPage extends Component {
+class AntdPage extends React.Component {
+
+    columns = [
+        {
+            title: 'title',
+            dataIndex: 'title',
+            width: 50
+        },
+        {
+            title: 'price',
+            dataIndex: 'price'
+        },
+        {
+            title: 'description',
+            dataIndex: 'description'
+        }
+    ];
+
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            books: [],
+            visible: false
+        };
+    }
+
+    async componentDidMount() {
+        const res = (await getBooks()).data;
+        this.setState({books: res});
+    }
+
+    toggleModal = () => {
+        this.setState({
+            visible: !this.state.visible
+        });
+    };
+
+    handleCancel = () => {
+        console.log('Class: AntdPage, Function: handleCancel, Line 43 (): ');
+        this.toggleModal();
+    };
+
+    handleOk = () => {
+        console.log('Class: AntdPage, Function: handleOk, Line 48 (): ');
+        this.toggleModal();
+    };
+
     render() {
-        const dataSource = [
-            {
-                key: '1',
-                name: 'Mike',
-                age: 32,
-                address: '10 Downing Street'
-            },
-            {
-                key: '2',
-                name: 'John',
-                age: 42,
-                address: '10 Downing Street'
-            }
-        ];
-
-        const columns = [
-            {
-                title: 'Name',
-                dataIndex: 'name',
-                key: 'name'
-            },
-            {
-                title: 'Age',
-                dataIndex: 'age',
-                key: 'age'
-            },
-            {
-                title: 'Address',
-                dataIndex: 'address',
-                key: 'address'
-            }
-        ];
-
         return (
-            <div>
-                <Table dataSource={dataSource} columns={columns}/>
-            </div>
+            <React.Fragment>
+                <Row>
+                    <Col span={5}>
+                        <div className={'col-5'}>
+                            <Table dataSource={this.state.books} columns={this.columns} pagination={false}
+                                   rowKey={'title'}/>
+                        </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Button onClick={this.toggleModal}>show modal</Button>
+                    <Modal
+                        title="Basic Modal"
+                        visible={this.state.visible}
+                        onCancel={this.handleCancel}
+                        footer={this.renderFooter()}
+                    >
+                        <p>Some contents...</p>
+                        <p>Some contents...</p>
+                        <p>Some contents...</p>
+                    </Modal>
+                </Row>
+            </React.Fragment>
         );
     }
+
+    renderFooter = () => {
+        return <React.Fragment>
+            <Button>btn 1</Button>
+            <Button>btn 2</Button>
+        </React.Fragment>;
+    };
 }
 
 export default AntdPage;
