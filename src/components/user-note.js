59d0ffd2b8884a3e9acb682d1314a7385e7a0605
alanhe <qianghe421@gmail.com>
2020-08-02 18:29:21 +0800

    test react.memo

--- src/components/user-note.js
+++ src/components/user-note.js
@@ -1,20 +1,7 @@
 import React from 'react';
 
-export class UserNote extends React.PureComponent {
-
-
-  constructor(props, context) {
-    super(props, context);
-  }
-
-  sayHello() {
-    alert('I am note!');
-  }
-
-  render() {
-    console.log('UserNote');
-    return (
-      <textarea placeholder={'write something'}>
-        </textarea>);
-  }
-}
+export const UserNote = React.memo(() => {
+  console.log('UserNote');
+  return <textarea placeholder={'write something'}>}
+        </textarea>;
+});
