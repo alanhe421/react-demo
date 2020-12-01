d9cf78697563351f3dafe5d16621092e7d8215e2
Alan He <alan@1991421.cn>
2020-12-01 14:16:10 +0800

    fix error

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,9 +1,8 @@
 import React from 'react';
 
 const home = () => {
-  const num = null;
   return <h1>home-111
-    111113&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;33{num.toString()}
+    111113&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
   </h1>;
 };
 export default home;
