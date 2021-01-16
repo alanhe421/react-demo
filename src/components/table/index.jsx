09d90ec44677bede3a32cd4223319ad9bd8fcd5d
Alan He <alan@1991421.cn>
2021-01-16 20:21:16 +0800

    test virtualizedtableforantd

--- src/components/table/index.jsx
+++ src/components/table/index.jsx
@@ -1,116 +1,185 @@
 import React from 'react';
-import { Table } from 'antd';
-import { VTComponents } from 'virtualizedtableforantd';
+import { Form, Input, InputNumber, Popconfirm, Table } from 'antd';
+import { setComponents, VTComponents } from 'virtualizedtableforantd';
 
-const valueEnum = {
-  0: 'close',
-  1: 'running',
-  2: 'online',
-  3: 'error'
-};
-
-const tableListDataSource = [];
-for (let i = 0; i < 2; i += 1) {
-  tableListDataSource.push({
-    key: i,
-    name: `TradeCode ${i}`,
-    status: valueEnum[Math.floor(Math.random() * 10) % 4],
-    updatedAt: Date.now() - Math.floor(Math.random() * 1000),
-    createdAt: Date.now() - Math.floor(Math.random() * 2000),
-    money: Math.floor(Math.random() * 2000) * i
+const data = [];
+for (let i = 0; i < 100; i++) {
+  data.push({
+    key: i.toString(),
+    name: `Edrward ${i}`,
+    age: 32,
+    address: `London Park no. ${i}`
   });
 }
-const columns = [
-  {
-    title: '标题',
-    dataIndex: 'name',
-    key: 'name'
-  },
-  {
-    title: '状态',
-    dataIndex: 'status',
-    initialValue: 'all',
-    filters: true,
-    onFilter: true,
-    valueType: 'select',
-    valueEnum: {
-      all: { text: '全部', status: 'Default' },
-      close: { text: '关闭', status: 'Default' },
-      running: { text: '运行中', status: 'Processing' },
-      online: { text: '已上线', status: 'Success' },
-      error: { text: '异常', status: 'Error' }
+const EditableContext = React.createContext();
+
+
+class EditableCell extends React.Component {
+  getInput = () => {
+    if (this.props.inputType === 'number') {
+      return <InputNumber />;
     }
-  },
-  {
-    title: '创建时间',
-    key: 'since',
-    dataIndex: 'createdAt',
-    valueType: 'dateTime'
-  },
-  {
-    title: '更新时间',
-    key: 'since2',
-    dataIndex: 'createdAt',
-    valueType: 'date'
-  },
-  {
-    title: '操作',
-    key: 'option',
-    width: 120,
-    valueType: 'option',
-    render: () => <button onClick={() => this.setState({ spin: true })}>
-      open loading
-    </button>
+    return <Input />;
+  };
+
+  renderCell = ({ getFieldDecorator }) => {
+    const {
+      editing,
+      dataIndex,
+      title,
+      inputType,
+      record,
+      index,
+      children,
+      ...restProps
+    } = this.props;
+    return (
+      <td {...restProps}>
+        {editing ? (
+          <Form.Item style={{ margin: 0 }}>
+            {getFieldDecorator(dataIndex, {
+              rules: [
+                {
+                  required: true,
+                  message: `Please Input ${title}!`
+                }
+              ],
+              initialValue: record[dataIndex]
+            })(this.getInput())}
+          </Form.Item>
+        ) : (
+          children
+        )}
+      </td>
+    );
+  };
+
+  render() {
+    return <EditableContext.Consumer>{this.renderCell}</EditableContext.Consumer>;
   }
-];
-
-class TablePage extends React.Component {
+}
 
 
-  constructor(props, context) {
-    super(props, context);
-    this.state = {
-      spin: true,
-      dataSource: new Array(1000).fill(null).map(() => ({
-        name: Math.random()
-      }))
-    };
+class EditableTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { data, editingKey: '' };
+    this.columns = [
+      {
+        title: 'name',
+        dataIndex: 'name',
+        width: '25%',
+        editable: true
+      },
+      {
+        title: 'age',
+        dataIndex: 'age',
+        width: '15%',
+        editable: true
+      },
+      {
+        title: 'address',
+        dataIndex: 'address',
+        width: '40%',
+        editable: true
+      },
+      {
+        title: 'operation',
+        dataIndex: 'operation',
+        render: (text, record) => {
+          const { editingKey } = this.state;
+          const editable = this.isEditing(record);
+          return editable ? (
+            <span>
+              <EditableContext.Consumer>
+                {form => (
+                  <a
+                    onClick={() => this.save(form, record.key)}
+                    style={{ marginRight: 8 }}
+                  >
+                    Save
+                  </a>
+                )}
+              </EditableContext.Consumer>
+              <Popconfirm title="Sure to cancel?" onConfirm={() => this.cancel(record.key)}>
+                <a>Cancel</a>
+              </Popconfirm>
+            </span>
+          ) : (
+            <a disabled={editingKey !== ''} onClick={() => this.edit(record.key)}>
+              Edit
+            </a>
+          );
+        }
+      }
+    ];
+  }
+
+  isEditing = record => record.key === this.state.editingKey;
+
+  cancel = () => {
+    this.setState({ editingKey: '' });
+  };
+
+  save(form, key) {
+    form.validateFields((error, row) => {
+      if (error) {
+        return;
+      }
+      const newData = [...this.state.data];
+      const index = newData.findIndex(item => key === item.key);
+      if (index > -1) {
+        const item = newData[index];
+        newData.splice(index, 1, {
+          ...item,
+          ...row
+        });
+        this.setState({ data: newData, editingKey: '' });
+      } else {
+        newData.push(row);
+        this.setState({ data: newData, editingKey: '' });
+      }
+    });
+  }
+
+  edit(key) {
+    this.setState({ editingKey: key });
   }
 
   render() {
-    return <Table
-      scroll={{ y: 600 }} // It's important for using VT!!! DO NOT FORGET!!!
-      columns={[
-        {
-          title: 'Name',
-          dataIndex: 'name'
-        },
-        {
-          title: 'Name1',
-          dataIndex: 'name1'
-        },
-        {
-          title: 'Name2',
-          dataIndex: 'name2'
-        }, {
-          title: 'Name3',
-          dataIndex: 'name3'
-        }, {
-          title: 'Name4',
-          dataIndex: 'name4'
-        }, {
-          title: 'Name5',
-          dataIndex: 'name5'
-        }, {
-          title: 'Name6',
-          dataIndex: 'name6'
-        }
-      ]}
-      components={VTComponents({ id: 1111 })}
-      dataSource={this.state.dataSource}
-      pagination={false}
-    />;
+    setComponents(1, { body: { cell: EditableCell } });
+    const columns = this.columns.map(col => {
+      if (!col.editable) {
+        return col;
+      }
+      return {
+        ...col,
+        onCell: record => ({
+          record,
+          inputType: col.dataIndex === 'age' ? 'number' : 'text',
+          dataIndex: col.dataIndex,
+          title: col.title,
+          editing: this.isEditing(record)
+        })
+      };
+    });
+
+    return (
+      <EditableContext.Provider value={this.props.form}>
+        <Table
+          components={VTComponents({ id: 1, debug: true })}
+          bordered
+          scroll={{ y: 500 }}
+          dataSource={this.state.data}
+          columns={columns}
+          rowClassName="editable-row"
+          pagination={false}
+        />
+      </EditableContext.Provider>
+    );
   }
 }
 
-export default TablePage;
+const EditableFormTable = Form.create()(EditableTable);
+
+export default EditableFormTable;
