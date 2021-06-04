2b4e867fd249ba5a283a5574405cdf3a8ad4e29e
Alan He <qianghe421@gmail.com>
2021-06-04 16:34:47 +0800

    测试react hooks

--- src/components/home/click-counter.jsx
+++ src/components/home/click-counter.jsx
@@ -4,10 +4,50 @@ import React from 'react';
 export default function ClickCounter() {
   const [count, setCount] = useState(0);
   const handleClick = () => setCount(count + 1);
+  const { user, callUser } = useCallUserApi(count);
   useEffect(() => {
-    document.title = `you click ${count} times`;
+    document.title = `click ${count} times`;
   }, [count]);
+
   return <div onClick={handleClick}>
     you click {count} times;
+    ////
+    userInfo:{JSON.stringify(user)}
+    <button onClick={() => callUser()}>
+      call user
+    </button>
   </div>;
+}
+
+
+function useCallUserApi(userId) {
+  const [user, setUser] = useState();
+  console.log(userId);
+
+
+  useEffect(() => {
+    console.log('init useCallUserApi');
+  }, []);
+
+  function callUser() {
+    new Promise((resolve) => {
+      window.setTimeout(() => {
+      }, 1000);
+      resolve({
+        userId: userId,
+        userName: 'alan he'
+      });
+    }).then((user) => {
+      setUser(user);
+    });
+  }
+
+  useEffect(() => {
+      callUser();
+    }
+    , []);
+  return {
+    user,
+    callUser
+  };
 }
