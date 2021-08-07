770f64cbbec739d4caec66949730f1651a5f66da
Alan He <qianghe421@gmail.com>
2021-08-07 16:07:05 +0800

    test react-hook-form

--- src/components/form/formPage.jsx
+++ src/components/form/formPage.jsx
@@ -1,113 +1,31 @@
-import React from 'react';
-import { Form } from 'antd/es';
-import Icon from 'antd/es/icon';
-import Input from 'antd/es/input';
-import Button from 'antd/es/button';
-import OperationDisabled from '../operation-disabled';
-import { setUserInfo } from '../../actions';
-import { connect } from 'react-redux';
-import { InputNumber } from 'antd';
+import React, { useCallback } from 'react';
+import { Button, Select } from 'antd';
+import { Controller, useForm } from 'react-hook-form';
 
-class FormPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      disabled: true,
-      username: 'hhhhh'
-    };
-    this.doSomething = this.doSomething.bind(this);
-  }
-
-  listener = function(e) {
-    console.log('will unmount');
-    e.returnValue = '';
-  };
-
-  render() {
-    const { getFieldDecorator } = this.props.form;
-
-    return (
-      <div>
-        <OperationDisabled disabled={false}>
-          <div>
-            test OperationDisabled
-          </div>
-          <Form className="login-form">
-            <Form.Item>
-              {getFieldDecorator('name', {
-                rules: [{ required: true, message: 'Please input your username!' }],
-                initialValue: this.state.username
-              })(
-                <Input
-                  prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                  placeholder="username"
-                  onBlur={this.onUsernameBlur}
-                />
-              )}
-            </Form.Item>
-            <Form.Item>
-              {getFieldDecorator('age', {
-                rules: [{ required: true, message: 'Please input your age!' }]
-              })(
-                <InputNumber
-                  prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                  placeholder="age"
-                />
-              )}
-            </Form.Item>
-          </Form></OperationDisabled>
-        <Button onClick={this.updateCheckboxDisabled}>update checkbox disabled status</Button>
-        <Button onClick={this.updateUsername}>update username</Button>
-        <Button onClick={this.onSubmit}>submit</Button>
-        <Button onClick={this.doSomething}>do something</Button>
-      </div>
-    );
-  }
-
-  onSubmit = () => {
-    this.props.form.validateFields();
-  };
-
-
-  updateCheckboxDisabled = () => {
-    this.setState({
-      disabled: !this.state.disabled
-    });
-  };
-
-  updateUsername = () => {
-    this.setState({
-      username: Math.random()
-    });
-  };
-
-  handleRememberChange = (e) => {
-    // e.target.checked = false;
-  };
-
-
-  doSomething() {
-    console.log(this.props.user);
-  }
-
-}
-
-const mapStateToProps = (state) => {
-  return {
-    user: state.user
-  };
+const { Option } = Select;
+const FormPage = () => {
+  const { register, reset, control, getValues } = useForm();
+  const handleSubmit = useCallback(() => {
+    console.log(getValues());
+  }, []);
+  return <div><Controller
+    control={control}
+    name={'name'}
+    render={({ field, fieldState, formState }) => <Select
+      style={{ width: 200 }}
+      placeholder="Select a person"
+      optionFilterProp="children"
+      {...field}
+    >
+      <Option value={'1'}>Jack</Option>
+      <Option value={'2'}>Lucy</Option>
+      <Option value={'3'}>Tom</Option>
+    </Select>}>
+  </Controller>
+    <div>
+      <Button onClick={handleSubmit}>submit</Button>
+    </div>
+  </div>;
 };
 
-const mapDispatchToProps = {
-  setUserInfo
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Form.create({
-  onValuesChange: (props, changedValues, allValues) => {
-    console.log(allValues);
-    props.setUserInfo(allValues);
-  }
-})(FormPage));
+export default FormPage;
