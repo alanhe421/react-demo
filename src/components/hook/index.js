f93b41dc33a83012ac4b46fc3abc7a14ad17658f
Alan He <qianghe421@gmail.com>
2021-08-15 20:14:16 +0800

    test ahooks

--- src/components/hook/index.js
+++ src/components/hook/index.js
@@ -1,43 +1,35 @@
-import React from 'react';
-
 /**
  * hook测试
  */
-import { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useRequest } from 'ahooks';
+import { getUserInfos } from '../../api';
 
 const HookTest = () => {
   const [type, setType] = useState('boy');
-  const { userList, run } = useFetchUserList(type);
-
-
-  useEffect(() => {
-    run();
-  }, [type]);
-
+  const { userList } = useFetchUserList(type);
   return <>
     'hello world'
-
     <button onClick={() => setType('girl')}>
       click
     </button>
+
+    {
+      JSON.stringify(userList || null)
+    }
   </>;
 };
 
 // 对于type，无论是基本数据类型还是对象类型，都会是最新值
-function useFetchUserList(type) {
-  const [userList, setUserList] = useState([]);
-  const run = useCallback(() => {
-    console.log(type);
-    return new Promise(resolve => {
-      window.setTimeout(() => {
-        resolve((new Array(100).fill().map(() => Math.random())));
-      }, 3000);
-    }).then(res => setUserList(res));
-  }, [type]);
+function useFetchUserList() {
+  const { loading, run, data } = useRequest(getUserInfos, {
+    manual: false
+  });
 
   return {
     run,
-    userList
+    userList: data,
+    loading
   };
 }
 
