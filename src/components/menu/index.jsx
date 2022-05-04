4308138f8ff20dbc54cc6d329a815c38bc575b9e
alanhe <qianghe421@gmail.com>
2022-05-04 15:56:38 +0800

    动态菜单内容

--- src/components/menu/index.jsx
+++ src/components/menu/index.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Menu, Item, Separator, Submenu, MenuProvider, useContextMenu } from 'react-contexify';
+import React, {useState} from 'react';
+import {Item, Menu, useContextMenu} from 'react-contexify';
 import 'react-contexify/dist/ReactContexify.css';
 
 
@@ -8,13 +8,16 @@ const MENU_ID = 'blahblah';
 
 const MenuTest = () => {
 
-  const { show } = useContextMenu({
+  const {show} = useContextMenu({
     id: MENU_ID
   });
+
+  const [hoveredId, setHoveredId] = useState(null);
 
   function handleContextMenu(event) {
     event.preventDefault();
     console.log(event);
+    setHoveredId(event.target.dataset.id);
     show(event, {
       props: {
         key: event.target.dataset.id
@@ -22,7 +25,7 @@ const MenuTest = () => {
     });
   }
 
-  const handleItemClick = ({ event, props }) => {
+  const handleItemClick = ({event, props}) => {
     console.log(props);
   };
 
@@ -30,6 +33,9 @@ const MenuTest = () => {
     <p data-id='1' onContextMenu={handleContextMenu}>node1</p>
     <p data-id='2' onContextMenu={handleContextMenu}>node2</p>
     <Menu id={MENU_ID}>
+      <div>
+        selectedId:{hoveredId}
+      </div>
       <Item onClick={handleItemClick}>上传</Item>
       <Item onClick={handleItemClick}>下载</Item>
     </Menu>
